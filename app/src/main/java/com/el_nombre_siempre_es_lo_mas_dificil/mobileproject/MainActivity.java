a258ed0aece7654baa3dd8d7e54eac20c4dbb533
otreblan <otreblain@gmail.com>
2020-07-04 10:59:17 -0500

    First commit

---
+++ app/src/main/java/com/el_nombre_siempre_es_lo_mas_dificil/mobileproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.el_nombre_siempre_es_lo_mas_dificil.mobileproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
